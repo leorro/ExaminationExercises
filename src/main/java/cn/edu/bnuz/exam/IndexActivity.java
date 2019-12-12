c73d0bc7c5f8b718de270138e5000afd289ef2fb
zhcxk1998 <464203147@qq.com>
2019-12-12 13:38:34 +0800

    fix: 修复排行榜信息显示顺序有误的问题

--- src/main/java/cn/edu/bnuz/exam/IndexActivity.java
+++ src/main/java/cn/edu/bnuz/exam/IndexActivity.java
@@ -1,22 +1,16 @@
 package cn.edu.bnuz.exam;
 
 import android.content.Intent;
-import android.content.res.TypedArray;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import cn.edu.bnuz.exam.adapter.ListViewAdapter;
-import cn.edu.bnuz.exam.dehelper.MyDBHelper;
+import cn.edu.bnuz.exam.dbhelper.MyDBHelper;
 import cn.edu.bnuz.exam.modals.RankInfo;
 
 public class IndexActivity extends AppCompatActivity {
@@ -29,21 +23,19 @@ public class IndexActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String exerciseName = intent.getStringExtra("exerciseName");
-        Log.d("IndexAcitivity", exerciseName);
 
         MyDBHelper myDBHelper = new MyDBHelper(getBaseContext(), "exercise.db", null, 1);
         SQLiteDatabase database = myDBHelper.getWritableDatabase();
         Cursor cursor = database.rawQuery("SELECT * FROM exercise_rank where exercise_name = '" + exerciseName + "' order by exercise_score desc", null);
 
         listView = (ListView) findViewById(R.id.listView);
-        ArrayList<HashMap<String, Object>> listItems = new ArrayList<>();
         final ArrayList<RankInfo> rankInfos = new ArrayList<>();
         cursor.moveToFirst();
 
-        rankInfos.add(new RankInfo("课程名称", "学生名称", "得分情况"));
+        rankInfos.add(new RankInfo("学生名称", "课程名称", "得分情况"));
         while (cursor.getPosition() != cursor.getCount()) {
-            String studentName = cursor.getString(1);
-            exerciseName = cursor.getString(2);
+            String studentName = cursor.getString(2);
+            exerciseName = cursor.getString(1);
             double exerciseScore = cursor.getDouble(3);
 
             rankInfos.add(new RankInfo(studentName, exerciseName, String.valueOf(exerciseScore)));
